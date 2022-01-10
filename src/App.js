939e680e1bfed6578781207d2e24eca01bcb13cd
tri/KUMA <trikuma@hotmail.com>
2022-01-10 22:18:51 +0800

    Setup redux.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
 import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
 
-      {/* App Body */}
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* <Widgets /> */}
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* <Widgets /> */}
+        </div>
+      )}
     </div>
   );
 }
